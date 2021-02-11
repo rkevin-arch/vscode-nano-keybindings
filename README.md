4233d7af6fa0f6de15888b368a2895e38f888edb
rkevin <rk@rkevin.dev>
2021-02-11 13:22:59 -0800

    initial release, cut and paste working

--- README.md
+++ README.md
@@ -1,70 +1,33 @@
-# nano-keybindings README
+# nano-keybindings
 
-This is the README for your extension "nano-keybindings". After writing up a brief description, we recommend including the following sections.
+This is a VS Code extension that lets you use keybindings from GNU nano.
+
+Note that this is a personal project used by me. This extension may be buggy. in case it destroys your project or clipboard contents accidentally, please do submit a bug report, but I'm not liable for any of the damage caused. Use at your own risk.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+This extension gives you nano-style cutting and pasting:
+- `ctrl+k` cuts.
+  - If you have text selected, this will act as your traditional cutting (`ctrl+x` in most editorss).
+  - Otherwise, if you have "cut to end" enabled, this will cut everything from your current cursor to the end of the line, excluding the newline.
+  - If you have "cut to end" disabled, this will cut the entire line your cursor is on. If you use `ctrl+k` multiple times, it will append each line into your clipboard.
+- `ctrl+u` pastes.
+- `alt+u` toggles "cut to end" mode (default: disabled)
 
-For example if there is an image subfolder under your extension project workspace:
+It also adds the following keybindings for default actions:
+- `ctrl+w` for searching
+- `ctrl+\` for replace
+- `alt+w` for next match
+- `alt+q` for previous match
+- `ctrl+shift+_` for going to a line
+- `ctrl+x` for closing a file
 
-\!\[feature X\]\(images/feature-x.png\)
+## To-do
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+The feature I needed the most was cut and paste, so I haven't implemented any of the other features yet. If you would like to see a feature added, please let me know! (or better yet, make a PR).
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+### 0.1.0
 
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release, with cut and paste.
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,74 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { Func } from 'mocha';
 import * as vscode from 'vscode';
+
+var cutToEnd = false;
+var cutLine = -1;
+var cutTextEditor: vscode.TextEditor | null = null;
+
+async function cut(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+	if (!textEditor.selection.isEmpty){
+		// if we selected something, just cut that
+		let text = textEditor.document.getText(textEditor.selection);
+		vscode.env.clipboard.writeText(text);
+		edit.delete(textEditor.selection);
+		return;
+	}
+	if (cutToEnd) {
+		// cut until end of line (replace clipboard contents)
+		let line = textEditor.document.lineAt(textEditor.selection.active.line);
+		let range = new vscode.Range(textEditor.selection.active, line.range.end);
+		let text = textEditor.document.getText(range);
+		vscode.env.clipboard.writeText(text);
+		edit.delete(range);
+		return;
+	}
+	// we should cut the entire line, and either add it to our current clipboard or start fresh
+	// in order to add to our current clipboard, we must:
+	// 1. have started a cut operation recently
+	// 2. we're still using the same editor
+	// 2. between then and now, we should not have modified the document
+	// 3. we should cut the same line as last time 
+	// (if we're cutting a different line, it's a new cut operation)
+	// the original "next" line would have the same line number as the line that was cut
+	// if cutLine = -1, there is no recent cut operation after the latest modification (will be reset to -1 on edit)
+	// else it should match our current line anyway
+	let range = new vscode.Range(textEditor.selection.active.line, 0, textEditor.selection.active.line + 1, 0);
+    let text = textEditor.document.getText(range);
+	vscode.window.showInformationMessage(cutLine.toString() + (cutTextEditor === textEditor ? "true" : "false"));
+	if (cutLine === textEditor.selection.active.line && cutTextEditor === textEditor) {
+		await vscode.env.clipboard.readText().then((str) => {
+			text = str + text;
+		});
+	}
+	vscode.env.clipboard.writeText(text);
+	// set cutLine after the delete so our edit won't reset it to -1
+	textEditor.edit((editBuilder) =>{
+		editBuilder.delete(range);
+	}).then(()=>{
+		cutLine = textEditor.selection.active.line;
+	});
+	cutTextEditor = textEditor;
+	return;
+}
+
+function toggleCutToEnd(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+	cutToEnd = !cutToEnd;
+	vscode.window.setStatusBarMessage("Cut to end " + (cutToEnd ? "enabled" : "disabled"), 3000);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nano-keybindings" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('nano-keybindings.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Nano keybindings!');
-	});
-
-	context.subscriptions.push(disposable);
+	var commands: { [name: string]: (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => void } = {
+		'cut': cut,
+		'toggleCutToEnd': toggleCutToEnd,
+	};
+	for (let key in commands) {
+		context.subscriptions.push(vscode.commands.registerTextEditorCommand('nano-keybindings.'+key, commands[key]));
+	}
+	vscode.workspace.onDidChangeTextDocument(() => {cutLine = -1; vscode.window.showInformationMessage("Reset");});
 }
 
 // this method is called when your extension is deactivated
